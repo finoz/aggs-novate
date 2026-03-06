67b133164b9ad1380bc3cc7df7e8347e9e0141a8
finoz <finoz.webdev@gmail.com>
2026-03-06 15:28:40 +0100

    deploy setup - update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,10 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$basePath = dirname(__DIR__);
+$publicPath = is_dir($basePath . '/public_html') ? $basePath . '/public_html' : $basePath . '/public';
+
+$app = Application::configure(basePath: $basePath)
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -19,3 +22,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+$app->usePublicPath($publicPath);
+
+return $app;
