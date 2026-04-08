c01faa4d067777c266ea367f8fbd9e504a4ad4e1
finoz <finoz.webdev@gmail.com>
2026-04-08 21:23:24 +0200

    update calendar

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -13,7 +13,7 @@
         </figure>
         <section class="splash-content">
             <p>Qui il tuo gruppo scout preferito.<br>Il sito -come vedi- è in manutenzione: stiamo lavorando per voi.</p>
-            <p>Nel frattempo: se hai bisogno di dare un'occhiata al calendario, 👉 <a href="{{ asset('attachments/temp-AGGS_calendario_v2.pdf') }}" target="_blank">eccolo qui!</a> 👈</p>
+            <p>Nel frattempo: se hai bisogno di dare un'occhiata al calendario, 👉 <a href="{{ asset('attachments/temp-AGGS_calendario_v3.pdf') }}" target="_blank">eccolo qui!</a> 👈</p>
             <p>Per altre varie ed eventuali, scrivici!<br><a target="_blank" class="link-accent" href="mailto:{{ config('mail.from.address') }}">{{ config('mail.from.address') }}</a> </p>
             <p class="note">Vostrissimi,<br>capi e matusa</p>
         </section>
