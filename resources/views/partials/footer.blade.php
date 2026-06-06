78b15918e15efc409121d78f7283a7d2a7847126
finoz <finoz.webdev@gmail.com>
2026-06-06 14:45:33 +0200

    added link kanti

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,5 +2,7 @@
     <h2 class="visually-hidden">Footer</h2>
     <div class="sitefooter-inner">
         <p>&copy; {{ date('Y') }} {{ config('app.name') }}</p>
+        <a href="https://docs.google.com/document/d/1QqMwXX4immOLuMr0-6Ssh-VIO4x-xm7sm_vFYpDbjLw/edit?tab=t.0" 
+        target="_blank" class="btn btn--loud btn--cantata">Canti bivacco X salamella 2026</a>
     </div>
 </footer>
